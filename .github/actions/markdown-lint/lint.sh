7ab79463def6d08ec35e8fc932db637b2db93933
AnHaiTrinh <anhaitrinh.work@gmail.com>
2025-06-24 09:20:03 +0700

    fix bash scripts

--- .github/actions/markdown-lint/lint.sh
+++ .github/actions/markdown-lint/lint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-set -e
-
-EXIT_CODE=0
 
 find . -type f -name "*.md" | while IFS= read -r file; do
     echo "--------------------------------"
     echo "Checking: $file"
     if ! proselint "$file"; then
-        EXIT_CODE=1
+        exit 1
     fi
 done
-
-exit "$EXIT_CODE"
